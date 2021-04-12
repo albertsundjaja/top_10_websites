cca7e4a2c96c634552827cd3c76eebe0c88cacdb
albertsundjaja <albertsundjaja@gmail.com>
2021-04-12 21:28:20 +1000

    Moved vue-frontend submodule to the main git

---
+++ src/vue-frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center justify-center flex-grow-1">
+        <h1 class="text-center">Top 10 Websites for Learning Guitar test</h1>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <Main />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Main from './pages/Main';
+
+export default {
+  name: 'App',
+
+  components: {
+    Main,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/vue-frontend/src/pages/Main.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-card>
+                    <v-card-title>ShredAcademy</v-card-title>
+                    <v-card-text>
+                        <p>Free guitar learning resource that have things like lick of the week for some amazing techniques.</p>
+                    </v-card-text>
+                    <youtube class="text-center" :video-id="'5olxYUmumVc'"></youtube>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data: () => ({
+
+    }),
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

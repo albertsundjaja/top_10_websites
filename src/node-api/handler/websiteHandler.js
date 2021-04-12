b18d60ee9207681cc5de3bd8a4a415ba0a5b0b49
albertsundjaja <albertsundjaja@gmail.com>
2021-04-12 20:45:04 +1000

    Add dummy data for websites

--- src/node-api/handler/websiteHandler.js
+++ src/node-api/handler/websiteHandler.js
@@ -1,9 +1,31 @@
+const dummyItems = [
+    {
+        "id": 1,
+        "title": "ShredAcademy",
+        "description": "Free guitar learning resource that have things like lick of the week for some amazing techniques.",
+        "asset": {
+            type: "video",
+            url: "https://www.youtube.com/watch?v=5olxYUmumVc&ab_channel=ShredAcademy"
+        }
+    },
+    {
+        "id": 2,
+        "title": "Justin Guitar",
+        "description": "A guitar learning website that includes some free and paid guitar lessons.",
+        "asset": {
+            type: "image",
+            url: "https://jtgt-static.b-cdn.net/images/thumbnails/BGC.gif"
+        }
+    },
+
+]
+
 /**
  * Get list of top 10 websites 
  * GET /list
  */
 exports.getList = (req, res) => {
-    res.send([]);
+    res.send(dummyItems);
 }
 
 /**
@@ -11,5 +33,5 @@ exports.getList = (req, res) => {
  * GET /item/:id
  */
 exports.getItem = (req, res) => {
-    res.send({});
+    res.send(dummyItems.find((item) => item.id == req.params.id));
 }

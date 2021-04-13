c99b3f931caa1de76f46d4a524dcb6c12f11e071
albertsundjaja <albertsundjaja@gmail.com>
2021-04-13 21:06:19 +1000

    Added cors plugin

--- src/node-api/server.js
+++ src/node-api/server.js
@@ -1,9 +1,11 @@
 // load .env file
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
 const router = require('./route/router');
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.use(router);

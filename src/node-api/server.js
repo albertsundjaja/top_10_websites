f8a4ca91f5c17186de5b104268727997751ad6c1
albertsundjaja <albertsundjaja@gmail.com>
2021-04-13 21:07:30 +1000

    Added comment on cors

--- src/node-api/server.js
+++ src/node-api/server.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 const router = require('./route/router');
 
 const app = express();
+
+// TODO: edit cors so it only allow our frontend in prod
 app.use(cors());
+
 const port = process.env.PORT || 3000;
 
 app.use(router);
